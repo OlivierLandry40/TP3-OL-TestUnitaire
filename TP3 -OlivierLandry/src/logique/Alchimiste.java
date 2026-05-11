c45f606ce833b75cd8ba175757a50ecf15ed9801
OlivierLandry40 <2538953@cegeplimoilou.ca>
2026-05-11 11:23:13 -0400

    Ajout des messages d'erreur dans la classes alchimiste

--- TP3 -OlivierLandry/src/logique/Alchimiste.java
+++ TP3 -OlivierLandry/src/logique/Alchimiste.java
@@ -2,54 +2,64 @@ package logique;
 
 import java.util.Random;
 
-public class Alchimiste
-{
+/**
+ * Author : Olivier Landry
+ * Ordre de conception : 3e
+ */
+
+public class Alchimiste {
     public static final int EXPERIENCE_POUR_NIVEAU_SUIVANT = 500;
     private String nom;
     private int niveau;
     private int experience;
 
-    public Alchimiste(String nom)
-    {
+    public Alchimiste(String nom) {
         this.setNom(nom);
         this.setNiveau(1);
         this.setExperience(0);
     }
 
-    public Alchimiste(String nom,int niveau)
-    {
+    public Alchimiste(String nom, int niveau) {
         this(nom);
         setNiveau(niveau);
     }
 
-    public String getNom()
-    {
+    public String getNom() {
         return this.nom;
     }
-    public int getNiveau()
-    {
+
+    public int getNiveau() {
         return this.niveau;
     }
-    public int getExperience()
-    {
+
+    public int getExperience() {
         return this.experience;
     }
 
-    private void setNom(String nom)
-    {
+    private void setNom(String nom) {
+
+        if (nom == null)
+            throw new IllegalArgumentException("Le nom ne peut pas être null.");
+
+        if (nom.length() < 6)
+            throw new IllegalArgumentException("Le nom doit contenir au moins 6 caractères.");
+
         this.nom = nom;
     }
-    private void setNiveau(int niveau)
-    {
+
+    private void setNiveau(int niveau) {
         this.niveau = niveau;
     }
-    private void setExperience(int experience)
-    {
+
+    private void setExperience(int experience) {
         this.experience = experience;
     }
 
-    public boolean fairePotion(Recette recette)
-    {
+    public boolean fairePotion(Recette recette) {
+
+        if (recette == null)
+            throw new IllegalArgumentException("La recette ne peut pas être null.");
+
         boolean estReussi = false;
         double tauxExperience = this.niveau * 0.05;
         double tauxEchec = (recette.getDifficulte() * 0.25) - tauxExperience;
@@ -58,10 +68,9 @@ public class Alchimiste
             tauxEchec = 0;
 
         Random random = new Random();
-        double jetSuccess = ((double) random.nextInt(1, 101))/100;
+        double jetSuccess = ((double) random.nextInt(1, 101)) / 100;
 
-        if (jetSuccess >= tauxEchec)
-        {
+        if (jetSuccess >= tauxEchec) {
             int nbExperience = recette.getPointExperience();
             this.ajouterExperience(nbExperience);
             estReussi = true;
@@ -70,12 +79,10 @@ public class Alchimiste
         return estReussi;
     }
 
-    private void ajouterExperience(int experience)
-    {
+    private void ajouterExperience(int experience) {
         this.setExperience(this.getExperience() + experience);
 
-        if(this.getExperience() >= EXPERIENCE_POUR_NIVEAU_SUIVANT)
-        {
+        if (this.getExperience() >= EXPERIENCE_POUR_NIVEAU_SUIVANT) {
             this.setNiveau(this.getNiveau() + 1);
             this.setExperience(this.getExperience() - EXPERIENCE_POUR_NIVEAU_SUIVANT);
             System.out.println("Vous êtes maintenant un alchimiste de niveau " + this.getNiveau());
