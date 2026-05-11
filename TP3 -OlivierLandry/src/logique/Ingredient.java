e5d48970ccec159fbb5c8f64df78b3bd882ab8fa
OlivierLandry40 <2538953@cegeplimoilou.ca>
2026-05-11 11:32:56 -0400

    Ajout des conditons à la classe ingrédient ainsi que la documentation

--- TP3 -OlivierLandry/src/logique/Ingredient.java
+++ TP3 -OlivierLandry/src/logique/Ingredient.java
@@ -1,12 +1,10 @@
 package logique;
 
-public class Ingredient
-{
+public class Ingredient {
     private String nom;
     private int prix;
 
-    public Ingredient(String nom, int prix)
-    {
+    public Ingredient(String nom, int prix) {
         this.setNom(nom);
         this.setPrix(prix);
     }
@@ -15,7 +13,18 @@ public class Ingredient
         return nom;
     }
 
+    /**
+     * Preéconditions;
+     *  - nom != null
+     *  - nom >= 6
+     * @param nom
+     */
+
     private void setNom(String nom) {
+        if (nom == null)
+            throw new IllegalArgumentException("Le nom ne peut pas être null.");
+        if (nom.length() < 6)
+            throw new IllegalArgumentException("Le nom doit contenir au moins 6 caractères.");
         this.nom = nom;
     }
 
@@ -23,13 +32,21 @@ public class Ingredient
         return prix;
     }
 
+    /**
+     * Préconditions :
+     * - prix > 0
+     *
+     * @throws IllegalArgumentException si la précondition n'est pas respectée
+     */
+
     private void setPrix(int prix) {
+        if (prix <= 0)
+            throw new IllegalArgumentException("Le prix doit être supérieur à 0.");
         this.prix = prix;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Ingredient{" + "nom=" + nom + ", prix=" + prix + '}';
     }
 }
